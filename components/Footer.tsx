ee995dc5a4ff928b7d0a760ce51d356b72dc85bc
Anderson-Reges <andersonreges76@gmail.com>
2023-08-24 15:08:45 -0300

    Fix: fixing footer name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,8 @@
 const Footer: React.FC = () => {
   return (
-
     <div className="flex justify-center h-[80px] items-center">
         <p className="text-second font-extralight">© 2022 - 2023 Anderson Reges · </p>
     </div>
-
   );
 }
 
